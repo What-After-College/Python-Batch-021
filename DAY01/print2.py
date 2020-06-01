cc0d1bb7ec64683553f92622f59379d99f286089
Neeraj Sharma <nsharma0619@gmail.com>
2020-06-02 00:09:10 +0530

    DAY01

---
+++ DAY01/print2.py
@@ -0,0 +1,156 @@
+
+# print("hello world")
+
+# print("neeraj", "sharma", sep="-")
+
+
+# print("neeraj", end=" ")
+# print("sharma", end="")
+
+# n=input()
+
+# print(n)
+
+
+# Data types
+#     int - integer - 5,1,6
+#     float - decimal - 5.0,2.1
+#     str - chars - "neeraj", "sharma", 'neeraj', 'a'
+#     bool - boolean - True, False
+
+# n = 5
+# print(type(n))
+
+# python does not have any range of its data type
+
+
+# name='''This file is created to teach my students.
+#     And i am very happy'''
+
+# print(name)
+
+# n=input()
+
+# print(type(n))
+
+
+# type casting -> converting one type of variable into another
+
+# n = int(n)
+
+# print(type(n))
+
+
+# n = float(n)
+
+# print(type(n))
+
+
+# n = 5
+# n = str(n)
+# print(type(n))
+
+
+# operator:
+# arthmatic operator:
+# add +
+# sub -
+# mul *
+# divide /  (5/2)=2.5
+# integer division (5//2) = 2
+# modulus % it gives us remainder  5%2=
+# pow **
+# == a=5 b=5 a==b
+# = assign a=5
+# and 
+# or
+
+# print(5//3)
+
+
+# print(8%3)
+
+# print(5%8)
+
+
+# print(5**3)
+# print(16**(1/2))
+
+
+# if else condition
+# n = int(input())
+
+# if(n>50000):
+#     print("good work")
+#     print("good salary")
+# else:
+#     print("work hard")
+
+# odd even 
+
+# n=int(input())
+# if(n%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+# a,b,c 
+
+
+# elif conditions
+
+# a=int(input()) #5
+# b=int(input()) #0
+# c=int(input()) #8
+
+# if(a>b and a>c):
+#     print(a)
+# elif(b>a and b>c):
+#     print(b)
+# else:
+#     print(c)
+
+
+# nested loops
+# print yes for all even if they are greater then 10 else print no
+
+# n=int(input())
+# if(n%2==0):
+#     if(n>10):
+#         print('yes')
+#     else:
+#         print("no")
+
+# else:
+#     print("no")
+
+
+
+# introduction to loop
+
+
+# types of loop
+
+# while
+# for
+
+# n=0
+# while(n<5):
+#     print(n)
+#     n+=1
+
+
+# n=n+1  ---> n+=1
+# n=n-1  ---> n-=1
+# n=n*2  ---> n*=2
+# n=n/3  ---> n/=3
+
+
+# n = int(input())
+# i=1
+# while(i<=n):
+#     print(i, end=" ")
+#     i+=1
+
+print("hello")
+
