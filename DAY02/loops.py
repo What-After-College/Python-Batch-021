b3895f2fc27d681d7f903f27d199809e461eda8a
Neeraj Sharma <nsharma0619@gmail.com>
2020-06-02 23:35:55 +0530

    DAY02

---
+++ DAY02/loops.py
@@ -0,0 +1,171 @@
+# types of loop:
+# for
+# while 
+
+
+
+# for i in "vaibhaiv":
+#     print(i)
+
+# counting vowels
+
+# n=0  #n is counting number of a in string
+# for i in "banana":
+#     if(i=='a'):
+#         print(n)
+#         n+=1
+        
+
+
+# print(count)
+
+
+# range(n) function
+
+# n=5
+
+# 0,1,2,3,4
+
+# range(1,9)
+
+# 1,2,3,4,5,6,7,8
+
+# range(2,7,2)
+
+# 2,4,6
+
+
+
+
+
+
+# for i in range(2,21,2):
+#     print(i)
+
+
+
+# print first ten even number
+
+# for i in range(2,21,2):
+#     print(i)
+
+
+
+# print first ten odd number
+
+# for i in range(1,20,2):
+#     print(i)
+
+
+# ctrl + /  for comment
+
+
+
+
+# -----------------while-----------------
+
+
+# n=1
+# while(n<20):
+#     n=n*2
+#     print(n)
+
+
+# print character of a string
+
+# s = 'prashant'
+
+# index=0
+# while(index<8):
+#     print(s[index])
+#     index+=1
+
+
+# you are given a number n you have find whether this number is prime or not 
+# if prime print yes
+# otherwise print no
+
+
+# index=index+1
+# index+=1
+
+
+
+# continue 
+# break
+
+
+# for i in range(15):
+#     print(i)
+#     if(i==5):
+#         break
+
+
+# for i in range(15):
+#     if(i==5):
+#         break
+
+# print(i)
+
+
+# prime or not
+
+# n = int(input())
+
+# for i in range(2,n):
+#     if(n % i == 0):
+#         break
+
+# if(i == n-1):
+#     print("it is a prime")
+
+# else:
+#     print("it is not prime")
+
+
+# n = int(input())
+# for i in range(2,n):
+#     if(n%i==0):
+#         print(n, "is not a Prime")
+#         break
+#     if(i == n-1):
+#         print("it is a prime")
+
+
+
+
+# nested loops
+
+# input n=6
+# 1
+# 12
+# 123
+# 1234
+# 12345
+# 123456
+
+# n = int(input())
+
+# for row in range(1,n+1):
+#     for j in range(1,row+1):
+#         print(j,end="")
+#     print('')
+
+
+# n = int(input())
+# for i in range(1,n+1):
+#     for j in range(1,i):
+#         print(j, end= "")
+#     print(i)
+
+
+
+
+
+
+
+
+
+
+
+
